da10295622a555d6a8a527ae7b69760213d1a40c
smitiwis <sistemas.luismi@gmail.com>
2020-10-04 17:23:25 -0500

    dando un mejor estilo de transparencia a los cards y arreglando algunas clases en html

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,34 +1,34 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
-	render(){
-			return(
-					<nav className="main-navbar">
-							<div className="navbar-contenedor">
-									<ul className="navbar-contenedor-menu">
-											<li className="menu--items activate">
-													<NavLink activeClassName="menu__active" className="link" to="/home">Home</NavLink> 
-											</li>
-											<li className="menu--items activate">
-													<NavLink activeClassName="menu__active" className="link" to="/proyectos">Proyectos</NavLink> 
-											</li>
-											<li className="menu--items activate">
-													<NavLink activeClassName="menu__active" className="link" to="/habilidades">Habilidades</NavLink> 
-											</li>
-											<li className="menu--items activate">
-													<NavLink activeClassName="menu__active" className="link" to="/formacion">Formación</NavLink> 
-											</li>
-											<li className="menu--items activate">
-													<NavLink activeClassName="menu__active" className="link" to="/contacto">Contacto</NavLink> 
-											</li>
-											<li className="menu--items activate">
-													<NavLink activeClassName="menu__active" className="link" to="/experiencia">Experiencia</NavLink> 
-											</li>
-									</ul>
-							</div>
-					</nav>
-			)
+	render() {
+		return (
+			<nav className="main-navbar">
+				<div className="navbar-contenedor">
+					<ul className="navbar-contenedor-menu">
+						<li className="menu--items activate">
+							<NavLink activeClassName="menu__active" className="link" to="/home">Home</NavLink>
+						</li>
+						<li className="menu--items activate">
+							<NavLink activeClassName="menu__active" className="link" to="/proyectos">Proyectos</NavLink>
+						</li>
+						<li className="menu--items activate">
+							<NavLink activeClassName="menu__active" className="link" to="/habilidades">Habilidades</NavLink>
+						</li>
+						<li className="menu--items activate">
+							<NavLink activeClassName="menu__active" className="link" to="/formacion">Formación</NavLink>
+						</li>
+						<li className="menu--items activate">
+							<NavLink activeClassName="menu__active" className="link" to="/contacto">Contacto</NavLink>
+						</li>
+						<li className="menu--items activate">
+							<NavLink activeClassName="menu__active" className="link" to="/experiencia">Experiencia</NavLink>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		)
 	}
 }
 
